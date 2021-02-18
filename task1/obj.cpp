3137160ec1b07bd183636911b6ff8becdf3d5ac9
Grenby <dm.fedorov.cc@gmail.com>
2021-02-19 00:28:16 +0300

    __task1__

--- task1/obj.cpp
+++ task1/obj.cpp
@@ -18,33 +18,18 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    // We first classify ("color") the surfaces by splitting the original surface
-    // along sharp geometrical features. This will create new discrete surfaces,
-    // curves and points.
-
-
-    // Angle between two triangles above which an edge is considered as sharp:
     double angle = 40;
 
     // For complex geometries, patches can be too complex, too elongated or too
     // large to be parametrized; setting the following option will force the
     // creation of patches that are amenable to reparametrization:
-    bool forceParametrizablePatches = false;
+    bool forceParametrizablePatches = true;
 
     // For open surfaces include the boundary edges in the classification process:
     bool includeBoundary = true;
 
-    // Force curves to be split on given angle:
-    double curveAngle = 180;
-
-    gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., includeBoundary,
-                                        forceParametrizablePatches);
-
-    // Create a geometry for all the discrete curves and surfaces in the mesh, by
-    // computing a parametrization for each one
+    gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., includeBoundary,forceParametrizablePatches);
     gmsh::model::mesh::createGeometry();
-
-    // Create a volume from all the surfaces
     std::vector<std::pair<int, int> > s;
     gmsh::model::getEntities(s, 2);
     std::vector<int> sl;
@@ -54,14 +39,17 @@ int main(int argc, char **argv)
 
     gmsh::model::geo::synchronize();
 
-    // We specify element sizes imposed by a size field, just because we can :-)
-    bool funny = false; // false;
-    int f = gmsh::model::mesh::field::add("MathEval");
-    if(funny)
-        gmsh::model::mesh::field::setString(f, "F", "2*Sin((x+y)/5) + 3");
-    else
-        gmsh::model::mesh::field::setString(f, "F", "4");
-    gmsh::model::mesh::field::setAsBackgroundMesh(f);
+//    // We specify element sizes imposed by a size field, just because we can :-)
+//    bool funny = false; // false;
+//    int f = gmsh::model::mesh::field::add("MathEval");
+//    if(funny)
+//        gmsh::model::mesh::field::setString(f, "F", "20*Sin((x+y)/5) + 3");
+//    else
+//        gmsh::model::mesh::field::setString(f, "F", ".1");
+//    gmsh::model::mesh::field::setAsBackgroundMesh(f);
+
+    gmsh::model::getEntities(s);
+    gmsh::model::mesh::setSize(s,0.01);
 
     gmsh::model::mesh::generate(3);
 
